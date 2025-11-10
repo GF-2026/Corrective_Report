65d2003201fe225a4597b0256084b536b4b692ec
MVM <luis.covarrubias.fv@gmail.com>
2025-11-10 11:18:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -574,7 +574,7 @@ function verProximoServicio() {
 }
 document.getElementById('sendEmailBtn').addEventListener('click', () => {
   const to = "tck@olimp0.com";
-  const subject = encodeURIComponent("Nuevo reporte preventivo");
+  const subject = encodeURIComponent("Nuevo reporte correctivo");
 
   const company = get('company');
   const folio = generateFolio('folio');
